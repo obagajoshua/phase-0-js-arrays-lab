56c1264607afdb49f614c2350c01e98b391dee92
Obaga Joshua <obaga.joshua@student.moringaschool.com>
2026-09-25 11:52:47 +0300

    inventory

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,12 +1,30 @@
 // Write your code here
 
-
-
 // Export the necessary parts for testing
+
+const products = ["Laptop", "Phone", "Headphones", "Monitor"];
+
+function logFirstProduct() {
+  console.log(products[0]);
+}
+
+function addProduct(productName) {
+  products.push(productName);
+}
+
+function updateProductName(index, newName) {
+  products[index] = newName;
+}
+
+function removeLastProduct() {
+  products.pop();
+}
+
 module.exports = {
-  logFirstProduct: typeof logFirstProduct !== 'undefined' ? logFirstProduct : undefined,
-  addProduct: typeof addProduct !== 'undefined' ? addProduct : undefined,
-  updateProductName: typeof updateProductName !== 'undefined' ? updateProductName : undefined,
-  removeLastProduct: typeof removeLastProduct !== 'undefined' ? removeLastProduct : undefined,
-  products
+ logFirstProduct,
+  addProduct,
+  updateProductName,
+  removeLastProduct,
+  products,
 };
+
